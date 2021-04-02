0423261acbd8d42b5f3730a6aef029acbf0ba59a
Anderson Santos <anderson.santos@iteris.com.br>
2021-04-02 20:51:48 -0300

    pipeline

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const app = express();
 const port = 3000;
 
-app.get("/healthcheck", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
